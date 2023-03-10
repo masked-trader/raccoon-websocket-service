e3ce4320197bf3e28f3dfe503ce8bbc250654139
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2023-03-10 20:32:47 +0100

    added http status code error check

--- src/api.py
+++ src/api.py
@@ -7,25 +7,32 @@ from settings import settings
 
 @functools.cache
 def internal_retrieve_connection_config(connection_id: str) -> dict:
-    return requests.get(
+    resp = requests.get(
         f"{settings.internal_api_base_url}/internal/config/connection/{connection_id}/",
         verify=settings.internal_ssl_verify,
-    ).json()
+    )
+
+    resp.raise_for_status()
+    return resp.json()
 
 
 async def internal_update_balance_data(connection_id: str, data: dict):
-    return requests.post(
+    resp = requests.post(
         f"{settings.internal_api_base_url}/internal/exchange/balance/sync/",
         headers={"X-Connection-Id": connection_id},
         verify=settings.internal_ssl_verify,
         json=data,
     )
 
+    resp.raise_for_status()
+
 
 async def internal_update_order_data(connection_id: str, data: dict):
-    return requests.post(
+    resp = requests.post(
         f"{settings.internal_api_base_url}/internal/exchange/order/sync/",
         headers={"X-Connection-Id": connection_id},
         verify=settings.internal_ssl_verify,
         json=data,
     )
+
+    resp.raise_for_status()
